53409c1967dd3860f051a08d9c9de1f46e6bd87a
Mohamed-Singer <mohamed.singer@zyda.com>
2025-04-14 19:33:06 +0200

    Update logging middleware to only run in development mode

--- server/index.js
+++ server/index.js
@@ -13,6 +13,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Only log detailed request information in development
 if (process.env.NODE_ENV !== 'production') {
   app.use((req, res, next) => {
     console.log(`\n[${new Date().toISOString()}] ${req.method} ${req.originalUrl}`);
